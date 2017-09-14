e282e4da94fce6b2c47a48bc5a6e996fd18ae922
mcmhav <mcmhav@gmail.com>
2017-09-14 16:22:39 +0200

    new logging and remove deprecated ovverride

--- android/src/main/java/com/zeppelin/background/react/BackgroundReactPackage.java
+++ android/src/main/java/com/zeppelin/background/react/BackgroundReactPackage.java
@@ -22,7 +22,8 @@ public class BackgroundReactPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
+    // Depreciated RN 0.47
+    // @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
--- android/src/main/java/com/zeppelin/background/react/helpers/LogH.java
+++ android/src/main/java/com/zeppelin/background/react/helpers/LogH.java
@@ -6,17 +6,39 @@ public class LogH {
 
   private static final String TAG = "RCTBGService";
 
-  public static void i(String message) {
-    Log.i(TAG, message);
+  public static void breakerTop() {
+    Log.i(TAG, "╔═════════════════════════════════════════════");
   }
 
+  public static void breaker() {
+    Log.i(TAG, "╠═════════════════════════════════════════════");
+  }
+
+  public static void breakerSmall() {
+    Log.i(TAG, "╟─────────────────────────────────────────────");
+  }
+
+  public static void breakerBottom() {
+    Log.i(TAG, "╚═════════════════════════════════════════════");
+  }
+
+  public static void empty() {
+    Log.i(TAG, "");
+  }
+
+  public static void i(String message) {
+    Log.i(TAG, "║ " + message);
+  }
+
+  public static void iList(String message) {
+    Log.i(TAG, "╟─ " + message);
+  }
 
   public static void d(String message) {
-    Log.d(TAG, message);
+    Log.d(TAG, "║ " + message);
   }
 
-
   public static void e(String message) {
-    Log.e(TAG, message);
+    Log.e(TAG, "║ " + message);
   }
 }
